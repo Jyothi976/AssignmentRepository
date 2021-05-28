1edcc4b0fcd245696c93c50d0e00d533009715c5
Jyothi L M1 <jyothi.l.m1@ibm.com>
2021-05-28 13:24:08 +0530

    Created c.py and Altered a.c and b.cpp

--- a.c
+++ a.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main() {
+   //to print Hello world!
    printf("Hello, World!");
    return 0;
 }
--- b.cpp
+++ b.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int main()
 {
+    //to print Hello world!
     cout<<"Hello World"; 
     return 0;
 }
